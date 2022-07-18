d6d8119f81f4dbf918b4a03da2bd76e72401d988
Admin <avshenjuk@mail.ru>
2022-07-18 19:29:35 +0500

    Создание файлов HW

--- src/Homework/Homework_1_Light.java
+++ src/Homework/Homework_1_Light.java
@@ -1,5 +1,19 @@
 package Homework;
 
 public class Homework_1_Light {
+    public static void main(String[] args) {
+        //Дано (переменные ниже менять нельзя)
+        String hi = "                Hello ";
+        String world = " WoRld!";
+        char newLine = '\n';
+        //Создать из трех переменных единую строку,
+        //затроить (Можно вызвать тольку одну команду System.out.println())
+        //Привести к правильному виду (Ниже) используя String.trim()
+        String result = hi.trim() + world.toLowerCase() + newLine;
+        System.out.println(result.repeat(3));
 
+
+
+
+    }
 }
